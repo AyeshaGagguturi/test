7c9a58b613381f96c33b88d733c7759cf5ef918a
AyeshaGagguturi <89046160+AyeshaGagguturi@users.noreply.github.com>
2022-03-17 15:57:21 -0400

    Create h.py

---
+++ h.py
@@ -0,0 +1,16 @@
+
+# Python program to demonstrate
+# main() function
+  
+  
+print("Hello")
+  
+# Defining main function
+def main():
+    print("hey there")
+  
+  
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
